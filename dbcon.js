2d9a8fbf394a11f8fe4a6b5b37f593828a6f2284
Humza Ahmed <hsa5037@gmail.com>
2019-05-28 21:33:45 -0400

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_ahmedhu',
-  password        : 'Booya321!',
+  password        : '',
   database        : 'cs340_ahmedhu'
 });
 module.exports.pool = pool;
