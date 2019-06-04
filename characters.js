6b8990df1401fa4de728c69c089a5b3dd243034d
SaphiraSkies <36237047+SaphiraSkies@users.noreply.github.com>
2019-06-04 10:51:38 -0400

    Update for character.js

--- characters.js
+++ characters.js
@@ -2,6 +2,17 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    function getPlanets(res, mysql, context, complete){
+        mysql.pool.query("SELECT id, name FROM homeplanets", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.planets = results;
+            complete();
+        });
+    }
+
     function getChars(res, mysql, context, complete){
     	mysql.pool.query("SELECT C.name, H.name, P.name, A.alignment FROM characters C INNER JOIN characters_powers CP ON CP.cid = C.id INNER JOIN powers P ON P.id = CP.pid INNER JOIN planets H ON H.id = C.homeplanet INNER JOIN alignment A ON A.id = C.alignment ORDER BY C.name ASC;", function(error, results, fields){
     		if(error){
@@ -18,9 +29,10 @@ module.exports = function(){
     	var context = {};
     	var mysql = req.app.get('mysql');
     	getChars(res, mysql, context, complete);
+        getPlanets(res, mysql, context, complete);
     	function complete(){
     		callbackCount++;
-    		if(callbackCount >= 1){
+    		if(callbackCount >= 2){
     			res.render('characters', context);
     		}
     	}
