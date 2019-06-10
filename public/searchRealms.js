27b0cd3bb2d8a9a2f5c4960bba4c65120f049393
Humza Ahmed <hsa5037@gmail.com>
2019-06-09 21:09:57 -0400

    Update searchRealms.js

--- public/searchRealms.js
+++ public/searchRealms.js
@@ -1,6 +1,7 @@
 function searchRealmsByName() {
     //get the first name 
     var search_string  = document.getElementById('search_string').value
-    //construct the URL and redirect to it
+    //construct the URL and redirect to it 
     window.location = '/realms/search/' + encodeURI(search_string)
+    
 }
